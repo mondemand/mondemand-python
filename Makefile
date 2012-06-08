79776d367b8b49565226fafd0659ed3fa25b22e9
Git RPM Builder <produser@openx.org>
2012-06-08 02:12:37 +0000

    Got setup.py working

--- Makefile
+++ Makefile
@@ -5,6 +5,6 @@ LIBS=`PKG_CONFIG_PATH=/usr/lib/pkgconfig pkg-config mondemand-4.0 --libs`
 all:
 	/opt/bin/swig -python mondemand.i
 	gcc -fPIC $(CFLAGS) -I/opt/include/python2.6 -c mondemand_wrap.c 
-	ld -shared mondemand_wrap.o -o _mondemand.so $(LIBS)
+	gcc -shared mondemand_wrap.o -o _mondemand.so $(LIBS)
 clean:
 	rm -rf *.o *.so mondemand.py *.pyc *_wrap.c
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for Mondemand binding
+"""
+
+from distutils.core import setup, Extension
+import commands, syslog
+
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in commands.getoutput("PKG_CONFIG_PATH=/usr/lib/pkgconfig pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+        if flag_map.has_key(token[:2]):
+	    kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+        else:
+            kw.setdefault('extra_link_args', []).append(token)
+    for k, v in kw.iteritems():
+        syslog.syslog(syslog.LOG_ERR, k)
+        syslog.syslog(syslog.LOG_ERR, str(v))
+        kw[k] = list(set(v))
+    kw['library_dirs'].append('/opt/lib64')
+    return kw
+
+mondemand_module = Extension('_mondemand',
+                             sources=['mondemand_wrap.c'],
+                             **pkgconfig('mondemand-4.0')
+                             )
+
+setup(name='mondemand',
+      version = '0.0.1',
+      author = "Keith Miller",
+      author_email = "keith.miller@openx.com", 
+      description = """Python bindings for Mondemand""",
+      url='http://www.mondemand.org',
+      ext_modules = [mondemand_module],
+      py_modules = ["mondemand"]
+      )

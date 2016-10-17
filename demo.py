40a086efa662401ce60980059547ea1c76ae2c1e
Luke Powers <los.powers@gmail.com>
2016-10-17 10:36:38 -0700

    updating python mondemand with performance traces and annotations; updated makefile and setup.py; updated readme with examples on how to use the client with lwes-event-printing-listener; added char** fix and documentation fix to mondemand.i swig file

--- demo.py
+++ demo.py
@@ -2,9 +2,9 @@ import mondemand
 
 # create the client
 client = mondemand.client_create("python_client")
-  
+
 # create a transport to send data
-transport = mondemand.transport_lwes_create("10.1.37.27", 20402, None, 0, 60)
+transport = mondemand.transport_lwes_create("127.0.0.1", 25552, None, 0, 60)
 
 # attach the transport to the client
 mondemand.add_transport(client, transport)
@@ -12,9 +12,12 @@ mondemand.add_transport(client, transport)
 # add some contextual information to the client
 mondemand.set_context(client, "cluster_id", "12345")
 
-# log some stats
-mondemand.stats_set(client, "filename.py", 1, "stat_1", 1234)
-mondemand.stats_inc(client, "filename.py", 1, "stat_2", 1)
+# set some stats
+mondemand.stats_set(client, "demo.py", 1, "stat_1", 1234)
+mondemand.stats_inc(client, "demo.py", 1, "stat_2", 1)
 
 # send it to the network
 mondemand.flush(client)
+
+# send some annotations
+mondemand.flush_annotation('', 1234567890, 'test', 'test desc', [], 0, client)
